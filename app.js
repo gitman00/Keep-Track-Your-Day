6ead5d7924734d64b0e9143b9c5b38e5dd960bae
gitman00 <ghimiremanish007@gmail.com>
2022-01-18 18:17:27 +0545

    Saved data into localStorage using js

--- app.js
+++ app.js
@@ -4,32 +4,47 @@ const Btn = document.querySelector('#btn-el');
 const ulEl = document.querySelector('#ul-el');
 const clear_bth = document.querySelector('#clear-el');
 
+
+let dataLocalStorage = JSON.parse(localStorage.getItem("Links"))
+console.log(dataLocalStorage)
+
+if (dataLocalStorage) {
+    list = dataLocalStorage;
+    displayInfo()
+}
+
 // display the info in the list after click in "Save Link" button 
 Btn.addEventListener('click', () => {
     list.push(inputEl.value);
     inputEl.value = "";
+
+    // storingb the list into local storage
+    localStorage.setItem("Links", JSON.stringify(list))
+
     displayInfo();
-    Display();
+    // Display();
+
 })
 
 // clearing the list after click in "Clear" button 
 clear_bth.addEventListener('click', () => {
     list = [];
     ulEl.innerHTML = "";
-    Display();
+    localStorage.clear()
+    // Display();
 })
 
 // mechanism to display or hide the clear button
-var Display = () => {
-    if (list.length > 0) {
-        clear_bth.style.display = "flex";
-    } else {
-        clear_bth.style.display = "none";
-    }
-}
+// var Display = () => {
+//     if (list.length > 0) {
+//         clear_bth.style.display = "flex";
+//     } else {
+//         clear_bth.style.display = "none";
+//     }
+// }
 
 // mechanism to display the info in unorderd list and saving the info in the list
-var displayInfo = () => {
+function displayInfo () {
     let items = "";
     for (let i = 0; i < list.length; i++) {
         items += `
